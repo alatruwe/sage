4c5b9bfe5a79059047411233b4c71abc6c6965a4
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-09-13 11:24:08 -0700

    added navbar content and style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,11 @@
 <header class="banner">
-  <div class="container">
+  <div class="navbar">
     <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
+      <button class="nav-button button">Try It Free</button>
     </nav>
   </div>
 </header>
