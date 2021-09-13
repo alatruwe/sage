91a250f67221c0a01f39e5573669690b2d380b34
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-09-13 10:48:29 -0700

    added footer content and style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,14 @@
 <footer class="content-info">
-  <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+  <div class="footer">
+    <div class="footer-block">
+      <div class="footer-content">
+        <h4>Manage and grow your private practice, all in one place.</h4>
+        <p>Free 30-day trial, no contracts, change anytime, tax deductible.</p>
+        <span class="footer-span"></span>
+        <button class="footer-button">Try It Free Now</button>
+      </div>
+
+    </div>
+
   </div>
 </footer>
