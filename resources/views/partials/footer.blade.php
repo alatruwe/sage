3c6ab3b6de44c873e52e5f595de53e0978bfb1fe
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-09-13 11:24:40 -0700

    refactor button styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,7 +5,7 @@
         <h4>Manage and grow your private practice, all in one place.</h4>
         <p>Free 30-day trial, no contracts, change anytime, tax deductible.</p>
         <span class="footer-span"></span>
-        <button class="footer-button">Try It Free Now</button>
+        <button class="button footer-button">Try It Free Now</button>
       </div>
 
     </div>
