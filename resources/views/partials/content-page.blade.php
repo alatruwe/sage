18773893f63ddcee4710d5b8535285724660dca6
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-09-13 12:43:36 -0700

    added intro section content and style

--- resources/views/partials/content-page.blade.php
+++ resources/views/partials/content-page.blade.php
@@ -1,2 +1,18 @@
 @php the_content() @endphp
-{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+{!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav">
+  <p>' . __('Pages:', 'sage'), 'after' => '</p>
+</nav>']) !!}
+<section>
+  <div class="hero">
+    <div class="hero_copy">
+      <h1>Tools, guides, and more to build your best practice.</h1>
+    </div>
+    <div class="hero-form">
+      <p>Our resources have helped thousands of your peers get better at their jobs. Get the latest in your inbox.</p>
+      <div class="hero-input">
+        <input class="email-input" type="email" name="email" placeholder="Enter your email to get started">
+        <button class="button">Join Our List Now</button>
+      </div>
+    </div>
+  </div>
+</section>
