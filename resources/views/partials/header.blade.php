a3892620f843c3f75283bb8284243a473198258d
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-09-13 15:03:34 -0700

    app refactor, cards as posts

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,4 +8,27 @@
       <button class="nav-button button">Try It Free</button>
     </nav>
   </div>
+  <section class="intro">
+    <div class="hero">
+      <div class="hero_copy">
+        <h1>Tools, guides, and more to build your best practice.</h1>
+      </div>
+      <div class="hero-form">
+        <p>Our resources have helped thousands of your peers get better at their jobs. Get the latest in your inbox.</p>
+        <div class="hero-input">
+          <input class="email-input" type="email" name="email" placeholder="Enter your email to get started">
+          <button class="button">Join Our List Now</button>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="filter-bar">
+    <div>All Resources</div>
+    <div>Telehealth</div>
+    <div>Guides</div>
+    <div>eBooks</div>
+    <div>Webinars</div>
+    <div>Classes</div>
+    <div>Tools</div>
+  </div>
 </header>
