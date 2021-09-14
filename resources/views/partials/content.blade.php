db26f961f5c7c0d03c934f5a8d156aad75391480
alatruwe <49768573+alatruwe@users.noreply.github.com>
2021-09-14 11:35:52 -0700

    updated cards content

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,9 +1,11 @@
 <article @php post_class() @endphp>
-  <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
-
-  </header>
-  <div class="entry-summary">
-    <p><?php the_field('test'); ?></p>
+  <div class="card">
+    <div class="entry-summary">
+      <div class="entry-content">
+        @php the_content() @endphp
+      </div>
+      <p><?php the_field('test'); ?></p>
+    </div>
   </div>
+
 </article>
